a4f35f3106e4b047836b96e168065ec61ffe20ec
weezzee <mpmedeiros17@gmail.com>
2018-04-03 21:27:44 -0300

    aula com banco c#

---
+++ c#/Etec.AulaRevisao.Interface/Etec.AulaRevisao.Interface/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Etec.AulaRevisao.Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpNasc = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(87, 182);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(79, 27);
+            this.btnCadastrar.TabIndex = 1;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(205, 182);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(79, 27);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(28, 39);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(196, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cargo";
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(28, 91);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(196, 20);
+            this.txtCargo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Salario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "RG";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(245, 91);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(111, 20);
+            this.txtSalario.TabIndex = 8;
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(28, 136);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(134, 20);
+            this.txtRG.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "CPF";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(193, 136);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(163, 20);
+            this.txtCPF.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Data Nascimento";
+            // 
+            // dtpNasc
+            // 
+            this.dtpNasc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNasc.Location = new System.Drawing.Point(245, 39);
+            this.dtpNasc.Name = "dtpNasc";
+            this.dtpNasc.Size = new System.Drawing.Size(111, 20);
+            this.dtpNasc.TabIndex = 13;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 247);
+            this.Controls.Add(this.dtpNasc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpNasc;
+
+    }
+}
+
---
+++ c#/Etec.AulaRevisao.Interface/Etec.AulaRevisao.Interface/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Etec.AulaRevisao.Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            MessageBox.Show("Campos foram Limpos", "ETEC ZL");
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            // validação
+            if (txtNome.Text == "" || txtCargo.Text == "")
+            {
+                MessageBox.Show("Todos os Campos devem ser preenchidos", "ETEC ZL");
+            }
+            else
+            {
+                //salvar informações no banco de dados
+                string conexao = "Persist Security Info=False;User ID=sa;Initial Catalog=AulaRevisao;Data Source=LAB-08-11;password=info211";
+                
+                //conectando ao sql server
+                SqlConnection conexaoBanco = new SqlConnection(conexao);
+                conexaoBanco.Open();
+                
+                //Colocando dados no sql
+                string comandoSQL = "INSERT INTO tbFuncionario VALUES('" + txtNome.Text +"','"
+                    +txtCargo.Text+ "','"
+                    + txtSalario.Text+ "','"
+                    +txtRG.Text+ "','"
+                    +txtCPF.Text+ "','"
+                    +dtpNasc.Text+ "')";
+
+                //execução de banco
+                SqlCommand execucaoSQL = new SqlCommand(comandoSQL, conexaoBanco);
+                execucaoSQL.ExecuteNonQuery();
+
+                //fechando a conexão com o banco
+                conexaoBanco.Close();
+
+                MessageBox.Show("Cadastro Realizado com Sucesso", "ETEC ZL");
+                LimparCampos();                
+            }
+        }
+        private void LimparCampos()
+        {
+            txtCargo.Text = "";
+            txtNome.Text = "";
+            txtCPF.Text = "";
+            txtRG.Text = "";
+            txtSalario.Text = "";
+            dtpNasc.Text = "";
+        }
+
+      
+
+
+        
+    }
+}
